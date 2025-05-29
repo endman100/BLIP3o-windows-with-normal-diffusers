8d471417c4063d2c061df6b871268d6ddf5631e6
endman100 <endman246738@gmail.com>
2025-05-29 14:54:25 +0800

    追加 install_requires

--- setup.py
+++ setup.py
@@ -2,6 +2,44 @@ from setuptools import setup, find_packages
 
 setup(
     name="blip3o",
-    version="0.1.0",
+    version="0.1.0.1",
     packages=find_packages(),
+    include_package_data=True,  # 啟用包含 MANIFEST.in 中指定的檔案
+    package_data={
+        "blip3o": [
+            "model/*",          # 包含 model 資料夾中的所有檔案
+        ]
+    },
+    install_requires=[
+        "tokenizers",
+        "sentencepiece",
+        "shortuuid",
+        "torch",
+        "torchvision",
+        "torchaudio",
+        "transformers==4.51.3",
+        "accelerate",
+        "peft",
+        "bitsandbytes",
+        "pydantic",
+        "markdown2",
+        "numpy",
+        "scikit-learn",
+        "gradio",
+        "gradio_client",
+        "requests",
+        "httpx",
+        "uvicorn",
+        "fastapi",
+        "einops",
+        "einops-exts",
+        "timm",
+        "ftfy",
+        "diffusers",
+        "datasets",
+        "tabulate",
+        "ninja",
+        "qwen_vl_utils",
+        "huggingface_hub",
+    ],
 )
